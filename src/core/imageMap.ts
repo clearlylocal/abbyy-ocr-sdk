949772d8836243907597da4ee6706151cd01af58
lionel-rowe <lionel.rowe@gmail.com>
2024-03-13 10:43:03 +0800

    Set minimum aspect ratio for soft tab as 1:1

--- src/core/imageMap.ts
+++ src/core/imageMap.ts
@@ -1,5 +1,6 @@
 import { load } from 'cheerio'
 import type { Cheerio, CheerioAPI, Element } from 'cheerio'
+import { assert } from 'std/assert/assert.ts'
 
 // https://support.abbyy.com/hc/en-us/articles/360017270080-Output-XML-document
 
@@ -14,17 +15,9 @@ const defaultImageMapOptions: ImageMapOptions = {
 export type ImageMap = { texts: PositionedText[] }
 
 /**
- * An element with numeric `l`, `t`, `r`, and `b` attributes, such as such as `<par>`, `<line>`, or `<charParams>`, that
- * can be converted to a {@link Rect}.
- */
-type $PositionedElement = Cheerio<Element> & {
-	readonly PositionedElement: unique symbol
-}
-
-/**
  * Text along with its positional coordinates as a rect.
  *
- * Note: Properties do _not_ correspond to the `<text />` and `<rect />` XML elements.
+ * Note: Properties do _not_ correspond to the FineReader XML `<text>` or `<rect>` elements.
  */
 type PositionedText = {
 	text: string
@@ -32,20 +25,46 @@ type PositionedText = {
 }
 
 /**
- * Note: Though its properties are the same as the ABBYY XML `<rect>` element's attributes, this does _not_ correspond
- * to `<rect>`. The `<rect>` elements are a low-level concept that do not yield usable results when used to locate
- * recognizable text areas. Instead, a `Rect` can correspond to any {@link $PositionedElement}, such as `<par>`,
- * `<line>`, or `<charParams>`.
+ * Represents the pixel coordinates of a rectangular area, corresponding to the bounding box of a
+ * {@link PositionedElement} such as `<line>` or `<charParams>`.
+ *
+ * Note: Though its name and properties are the same as those of the FineReader XML `<rect>` element, this does _not_
+ * correspond to `<rect>`. The `<rect>` elements are a low-level concept that do not yield meaningful results when used
+ * to locate text areas.
  */
-type Rect = {
+class Rect {
 	/** x coordinate of left border */
-	l: number
+	public l: number
 	/** y coordinate of top border */
-	t: number
+	public t: number
 	/** x coordinate of right border */
-	r: number
+	public r: number
 	/** y coordinate of bottom border */
-	b: number
+	public b: number
+
+	get width() {
+		return this.r - this.l
+	}
+
+	get height() {
+		return this.b - this.t
+	}
+
+	constructor({ l, t, r, b }: { l: number; t: number; r: number; b: number }) {
+		this.l = l
+		this.t = t
+		this.r = r
+		this.b = b
+	}
+
+	static merge(rects: Rect[]): Rect {
+		return new Rect({
+			l: Math.min(...rects.map((r) => r.l)),
+			t: Math.min(...rects.map((r) => r.t)),
+			r: Math.max(...rects.map((r) => r.r)),
+			b: Math.max(...rects.map((r) => r.b)),
+		})
+	}
 }
 
 export function imageMap(xml: string, options?: Partial<ImageMapOptions>): ImageMap {
@@ -55,25 +74,46 @@ export function imageMap(xml: string, options?: Partial<ImageMapOptions>): Image
 	return { texts }
 }
 
-function notNullish<T>(x: T): x is Exclude<T, null | undefined> {
-	return x != null
+/**
+ * An element with pixel coordinates that can be converted to a {@link Rect} via the {@link getRect} function
+ */
+type PositionedElement = Element & {
+	readonly tagName: PositionedElementTagName
+}
+
+/**
+ * Elements with required integer `l`, `t`, `r`, and `b` attributes, per the [FineReader 10.0 XML Schema](
+ * 	https://fr7.abbyy.com/FineReader_xml/FineReader10-schema-v1.xml
+ * )
+ */
+type PositionedElementTagName = typeof positionedElementTagNames[number]
+const positionedElementTagNames = ['line', 'charParams'] as const
+function positioned(el: Element) {
+	const { tagName } = el
+	assert(
+		positionedElementTagNames.includes(tagName as PositionedElementTagName),
+		`Invalid positioned element: ${tagName}`,
+	)
+	return el as PositionedElement
 }
 
 function getTexts($: CheerioAPI, rectType: RectType): PositionedText[] {
 	switch (rectType) {
 		case 'paragraph': {
 			return [...$('par')].flatMap((par) => {
-				const linesGroups = [...$(par).find('line')].map((x) =>
-					getPositionedTexts($, $(x) as $PositionedElement)
-				)
+				const linesGroups = [...$(par).find('line')].map((x) => getPositionedTexts($, $(positioned(x))))
 
 				return linesGroups.every((g) => g.length === 1) ? mergeLines(linesGroups.flat()) : linesGroups.flat()
 			}).filter(notNullish)
 		}
 		case 'line': {
-			return [...$('line')].flatMap((x) => getPositionedTexts($, $(x) as $PositionedElement))
+			return [...$('line')].flatMap((x) => getPositionedTexts($, $(positioned(x))))
 		}
 	}
+}
+
+function notNullish<T>(x: T): x is Exclude<T, null | undefined> {
+	return x != null
 }
 
 const nonSpaceDelimitedScripts = ['Han', 'Hiragana', 'Katakana', 'Thai', 'Khmer', 'Lao', 'Myanmar', 'Javanese'] as const
@@ -103,41 +143,29 @@ function mergeLines(lines: PositionedText[]): PositionedText | null {
 
 	return lines.length === 1 ? lines[0] : {
 		text: allText,
-		rect: mergeRects(lines.map((l) => l.rect)),
+		rect: Rect.merge(lines.map((l) => l.rect)),
 	}
 }
 
-function getRect($el: $PositionedElement): Rect {
+function getRect($el: Cheerio<PositionedElement>): Rect {
 	const l = Number($el.attr('l'))
 	const t = Number($el.attr('t'))
-	const b = Number($el.attr('b'))
 	const r = Number($el.attr('r'))
+	const b = Number($el.attr('b'))
 
-	return { l, t, b, r }
+	return new Rect({ l, t, r, b })
 }
 
-function mergeRects(rects: Rect[]): Rect {
-	return {
-		l: Math.min(...rects.map((r) => r.l)),
-		t: Math.min(...rects.map((r) => r.t)),
-		b: Math.max(...rects.map((r) => r.b)),
-		r: Math.max(...rects.map((r) => r.r)),
-	}
-}
-
-function getAspectRatio($el: $PositionedElement) {
-	const { l, t, b, r } = getRect($el)
-	const width = r - l
-	const height = b - t
-
+function getAspectRatio($el: Cheerio<PositionedElement>) {
+	const { width, height } = getRect($el)
 	return width / height
 }
 
-function getText($char: $PositionedElement) {
+function getText($char: Cheerio<PositionedElement>) {
 	if ($char.attr('isTab')) return '\t'
 
 	const text = $char.text()
-	const isSoftTab = text.trim() === '' && getAspectRatio($char) > 2.5
+	const isSoftTab = text.trim() === '' && getAspectRatio($char) > 1
 
 	return isSoftTab ? '\t' : text
 }
@@ -146,20 +174,20 @@ function getText($char: $PositionedElement) {
  * @param $ - Cheerio context
  * @param $line - XML `<line>` element
  */
-function getPositionedTexts($: CheerioAPI, $line: $PositionedElement): PositionedText[] {
-	const { l, t, b, r } = getRect($line)
+function getPositionedTexts($: CheerioAPI, $line: Cheerio<PositionedElement>): PositionedText[] {
+	const { l, t, r, b } = getRect($line)
 
-	const positionedTexts: PositionedText[] = [{ text: '', rect: { l, t, b, r } }]
+	const positionedTexts: PositionedText[] = [{ text: '', rect: new Rect({ l, t, r, b }) }]
 
 	for (const c of $line.find('charParams')) {
-		const $char = $(c) as $PositionedElement
+		const $char = $(positioned(c))
 		const positionedText = positionedTexts.at(-1)!
 
 		const text = getText($char)
 
 		if (text === '\t') {
 			positionedText.rect.r = Number($char.attr('l'))
-			positionedTexts.push({ text: '', rect: { l: Number($char.attr('r')), t, b, r } })
+			positionedTexts.push({ text: '', rect: new Rect({ l: Number($char.attr('r')), t, r, b }) })
 		} else {
 			positionedText.text += text
 		}
